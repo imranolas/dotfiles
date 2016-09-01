c0dab5546a47e4cddde644f33ab0bee14fc84a49
Imran <imran@thoughtmachine.net>
2016-09-01 15:20:23 +0100

    Add atom package list

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,9 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+if $(apm &>/dev/null)
+then
+  alias apm-install="apm install --packages-file ~/.atom/.packages"
+  alias apm-save="apm list --installed --bare > ~/.atom/.packages"
+fi
