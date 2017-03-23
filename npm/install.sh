6012a5cf84790852127382230ad2daec0675fee3
imranolas <imran.sulemanji@gmail.com>
2017-03-23 13:45:55 +0000

    Install react native

--- npm/install.sh
+++ npm/install.sh
@@ -3,5 +3,8 @@
 if test ! $(which node)
 then
   echo "  Installing node for you."
-  brew install node > /tmp/node-install.log
+  brew install node yarn > /tmp/node-install.log
 fi
+
+echo "  Installing node packages."
+npm install -g react-native-cli
